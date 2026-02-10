af6b7b72af41c4f91be917580950a181a0fc5831
aswathi603 <aswathipp17@gmail.com>
2026-02-10 20:01:07 +0530

    Monotonic Stack - Daily Temperature

---
+++ Python/Stack/Monotonic Stack I/Daily Temperature.py
@@ -0,0 +1,48 @@
+'''
+
+Question Link :https://leetcode.com/problems/daily-temperatures/?envType=problem-list-v2&envId=dsa-linear-shoal-monotonic-stack
+
+Q2. Daily Temperatures
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+ 
+
+Constraints:
+
+1 <= temperatures.length <= 105
+30 <= temperatures[i] <= 100
+
+'''
+
+from git import List
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n=len(temperatures)
+        res = [0]*n
+        stack = []
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev = stack.pop()
+                res[prev] = i - prev    
+            stack.append(i)
+        return res

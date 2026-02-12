6e78a652b0b0cb67af63549ff50be56923fbb9a5
aswathi603 <aswathipp17@gmail.com>
2026-02-12 19:51:50 +0530

    Update

--- Python/Stack/Monotonic Stack I/Final Prices With a Special Discount in a Shop.py
+++ Python/Stack/Monotonic Stack I/Final Prices With a Special Discount in a Shop.py
@@ -14,7 +14,6 @@ There is a special discount for items in the shop. If you buy the ith item, then
 
 Return an integer array answer where answer[i] is the final price you will pay for the ith item of the shop, considering the special discount.
 
- 
 
 Example 1:
 
